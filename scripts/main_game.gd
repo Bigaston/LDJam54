7bd260cd05cfa66542365f00790a745a12e01864
Bigaston <bigaston1@gmail.com>
2023-09-30 18:11:09 +0200

    Placement des meubles

--- scripts/main_game.gd
+++ scripts/main_game.gd
@@ -22,7 +22,7 @@ func _process(delta):
 	if furniture_to_place == null:
 		if selected_case != null:
 			selector.visible = true
-			selector.position = Vector3(selected_case.x + 0.5, 1, selected_case.y + 0.5)		
+			selector.position = Vector3(selected_case.x, 1, selected_case.y)		
 		else:
 			selector.visible = false
 	else:
@@ -30,14 +30,25 @@ func _process(delta):
 		
 		if selected_case != null:
 			furniture_to_place.visible = true
-			furniture_to_place.position = Vector3(selected_case.x + 0.5, 0.5, selected_case.y + 0.5)
+			furniture_to_place.position = Vector3(selected_case.x, 0.5, selected_case.y)
 		else:
 			furniture_to_place.visible = false
 			
-	# Rotation
+	# Inputs
 	if Input.is_action_just_pressed("furniture_rotate"):
 		furniture_to_place.rotation.y += deg_to_rad(90)
 		current_rotation = (current_rotation+90) % 360
+		
+	if Input.is_action_just_pressed("furniture_cancel"):
+		cancel_furniture_placement()
+		
+	if Input.is_action_just_pressed("furniture_place"):
+		if furniture_to_place != null:
+			var mesh = furniture_to_place.get_children()
+			set_material_of_furniture(mesh[0], null)
+			
+			furniture_to_place = null
+			last_frame_correct_status = null
 		
 	# Check if position is correct
 	if furniture_to_place != null && selected_case != null:
@@ -47,22 +58,16 @@ func _process(delta):
 				
 				var mesh = furniture_to_place.get_children()
 				
-				var nb_materials = mesh[0].get_surface_override_material_count()
-				
-				for mat in range(0, nb_materials):
-					(mesh[0] as MeshInstance3D).set_surface_override_material(mat, transparent_mat)
+				set_material_of_furniture(mesh[0], transparent_mat)
 		else:
 			if last_frame_correct_status != false:
 				last_frame_correct_status = false
 				
 				var mesh = furniture_to_place.get_children()
-				
-				var nb_materials = mesh[0].get_surface_override_material_count()
-				
-				for mat in range(0, nb_materials):
-					(mesh[0] as MeshInstance3D).set_surface_override_material(mat, transparent_error_mat)
+				set_material_of_furniture(mesh[0], transparent_error_mat)
 		
 func check_if_furniture_correct():
+	# TODO : Remplacer par la detection native de godot
 	if furniture_to_place == null or selected_case == null:
 		return
 	
@@ -105,7 +110,20 @@ func _physics_process(delta):
 		selected_case = Vector2(round(found_position.x), round(found_position.z))		
 
 func _on_ui_furniture_choosed(furniture):
-	print(furniture)
+	cancel_furniture_placement()
 	
 	furniture_to_place = furniture
 	add_child(furniture_to_place)
+
+func cancel_furniture_placement():
+	if furniture_to_place != null:
+		last_frame_correct_status = null
+		
+		furniture_to_place.queue_free()
+		furniture_to_place = null
+
+func set_material_of_furniture(mesh, material):
+	var nb_materials = mesh.get_surface_override_material_count()
+	
+	for mat in range(0, nb_materials):
+		(mesh as MeshInstance3D).set_surface_override_material(mat, material)
