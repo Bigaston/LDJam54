6944184f1e52cf9be6a8c6b518e0df1177ca3080
Bigaston <bigaston1@gmail.com>
2023-10-01 15:55:38 +0200

    Ajout des meubles manquants

--- scripts/Level.gd
+++ scripts/Level.gd
@@ -1,19 +1,25 @@
 extends Resource
 class_name Level
 
-enum FurnitureType {BED, TABLE, SIT, STORAGE}
+enum FurnitureType {BED, TABLE, SIT, STORAGE, DESK, LIGHT, TV}
 static var FurnitureTypeName = {
 	FurnitureType.BED: "bed",
 	FurnitureType.TABLE: "table",
 	FurnitureType.SIT: "place to sit",
-	FurnitureType.STORAGE: "storage"
+	FurnitureType.STORAGE: "storage",
+	FurnitureType.DESK: "desk",
+	FurnitureType.LIGHT: "light",
+	FurnitureType.TV: "TV cabinet"
 }
 
 static var FurnitureTypeNamePlural = {
 	FurnitureType.BED: "beds",
 	FurnitureType.TABLE: "tables",
 	FurnitureType.SIT: "places to sit",
-	FurnitureType.STORAGE: "storages"
+	FurnitureType.STORAGE: "storages",
+	FurnitureType.DESK: "desks",
+	FurnitureType.LIGHT: "light",
+	FurnitureType.TV: "TV cabinets"
 }
 
 @export var available_furnitures: Array[PackedScene]
