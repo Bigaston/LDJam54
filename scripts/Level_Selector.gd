995b43424391d0d74fb1759e356a370af8fd7cf1
Bigaston <bigaston1@gmail.com>
2023-10-01 23:28:23 +0200

    Ajout du level3

--- scripts/Level_Selector.gd
+++ scripts/Level_Selector.gd
@@ -6,8 +6,6 @@ func _on_level_1_pressed():
 	var level = load("res://ressources/levels/Level1.tres")
 	
 	choose_level.emit(level)
-	
-
 
 func _on_level_2_pressed():
 	var level = load("res://ressources/levels/Level2.tres")
@@ -15,7 +13,6 @@ func _on_level_2_pressed():
 	choose_level.emit(level)
 
 func _on_level_3_pressed():
-#	var level = load("res://ressources/levels/Level3.tres")
-#
-#	go_to_level(level)
-	pass
+	var level = load("res://ressources/levels/Level3.tres")
+
+	choose_level.emit(level)
